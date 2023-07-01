3a04d57c6afbff935d969548635eeaffd12ba176
kenyangitonga <89454432+blacklightske@users.noreply.github.com>
2023-07-01 14:00:46 +0300

    gr
    add data to slider and data in blog page

--- src/Data/BlogData.js
+++ src/Data/BlogData.js
@@ -15,10 +15,10 @@ export const BlogData = [
     blogText: ` Proper handling of green waste from your house compound is crucial for maintaining cleanliness and promoting a healthy environment. Key steps include segregating green waste from other waste types, establishing a composting system for organic waste such as grass clippings, leaves, and vegetable scraps, practicing grasscycling by leaving grass clippings on the lawn as natural mulch, utilizing mulching techniques to improve soil health, and exploring local green waste collection or community composting programs. Additionally, consider yard waste recycling centers or professional services for larger or more complex green waste management needs. Adhering to local regulations and guidelines ensures responsible disposal. By implementing these strategies, you can effectively handle green waste and contribute to a cleaner and more sustainable house compound environment.
 
 
-    Handling green waste from your house compound is important to maintain cleanliness and promote a healthy environment. Here are some tips on how to handle house compound green waste:
+     Handling green waste from your house compound is important to maintain cleanliness and promote a healthy environment. Here are some tips on how to handle house compound green waste:
     Segregation: Separate green waste from other types of waste, such as plastics or non-biodegradable materials. Create a designated area or container specifically for green waste to ensure proper segregation.
 Composting: Establish a composting system in your house compound. Use a compost bin or pile to collect green waste such as grass clippings, leaves, trimmings from shrubs or plants, and vegetable scraps. Add brown materials like dried leaves or shredded newspaper to create a balanced carbon-to-nitrogen ratio. Regularly turn the compost to facilitate decomposition. Avoid adding meat, dairy products, or oily foods to the compost, as they may attract pests.
-Grasscycling: Instead of bagging grass clippings after mowing, practice grasscycling. Leave the clippings on the lawn as natural mulch. They will decompose and provide nutrients to the soil, reducing the need for synthetic fertilizers.
+Grasscycling: Instead of bagging grass clippings after mowing, practice grasscycling.Leave the clippings on the lawn as natural mulch. They will decompose and provide nutrients to the soil, reducing the need for synthetic fertilizers.
 Mulching: Use a mulching mower or shredder to chop leaves, small branches, and other green waste into smaller pieces. Apply this mulch around plants and trees in your house compound to retain moisture, suppress weeds, and improve soil health.
 Local Green Waste Collection: Check if your local municipality or waste management services offer green waste collection. They may provide designated bins or bags for collecting green waste, which will be picked up for composting or proper disposal.
 Community Composting: Explore community composting initiatives in your area. Some communities have centralized composting facilities or composting programs where residents can drop off their green waste for composting on a larger scale.
@@ -93,4 +93,93 @@ Remember, food safety is crucial to protect the health of your customers and mai
     By implementing these strategies, you can effectively manage your home food intake, reduce food waste, and contribute to a more sustainable lifestyle`,
     link: "",
   },
-];
+
+{
+  id: "gcks6543je",
+  heading: "Waste management in Kenya",
+  date: "January 19, 2023",
+  blogText: `Waste Segregation
+  Most of the waste is generated at household, market places, cities, towns, institutions and industrial zones 
+ • Very few households segregate waste at the household level
+  • There is minimal waste segregation at source within the CBD areas, industries, institutions in most towns/cities
+  • There is considerable segregation of biomedical waste 
+ • Recovery of recyclable items like plastics, papers, glass, and metals is done by an increasing number of informal groups 
+ Collection and Transportation 
+ • Waste in the CBDs is largely collected by the County Governments while private operators dominate collection in residential areas at a fee. 
+ • Waste collection in low-income and informal settlements is mainly done by organized groups and CBOs • Waste collectors obtain permits from the County Governments to collect waste from designated
+  • NEMA issues annual licenses to waste transporters in accordance with the provisions of the waste management regulations of 2006. However, some waste transportation vehicles operate illegally as they do not meet NEMA requirements.
+ 
+ 
+ 
+ 
+ Waste Treatment 
+ • Waste treatment technologies have not been fully embraced in the country however there are on-going efforts to enhance waste treatment practices.
+  • Recyclable materials comprise 50 – 80% of the general waste stream;
+ • Several industries exist that receive recovered materials such as paper, polythene, plastics, glass, scrap metals, used oil, e-waste and waste tires for recycling. There is low public awareness of these facilities and hence majority have not achieved optimal operations; 
+ • A few composting facilities exist especially in horticultural farms;
+  • Thermal treatment of waste by use of incinerators and cement kilns is increasingly being adopted. However, most incinerators do not comply with the requirements of the Third Schedule of the waste management regulations of 2006;
+ 
+ Waste Disposal 
+ • Most of the municipal and domestic waste generated is disposed of in open dumpsites across the country. Although this is not a recommended practice it is the most common practice
+  • Biomedical waste is largely disposed of through incineration and rudimentary kilns;
+  • Condemned, damaged, or expired goods are disposed of through incineration or in the cement kilns 
+ • The existing incineration facilities have been largely burners and kilns and do not meet the requirements stipulated in the Third schedule of the waste management regulations of 2006 
+ • Most of the workforce operating these disposal sites have minimal or no training on how to manage these facilities.`,
+link:"",
+},
+{
+  id: "gcks6543jf",
+  heading: "Green Waste Management Solutions in Kenya",
+  date: "January 19, 2023",
+  blogText: `
+  As the world grapples with environmental challenges, Kenya has been taking strides towards sustainable waste management practices, particularly in dealing with green waste. Green waste refers to organic waste materials generated from plants, such as grass clippings, leaves, branches, and other garden waste. With an increasing focus on environmental conservation and the need to reduce greenhouse gas emissions, various innovative solutions have emerged in Kenya to address the management of green waste. In this blog post, we will explore some existing solutions that are making a positive impact in dealing with green waste in Kenya.
+   
+  1. Composting:
+  Composting is a widely practiced solution for managing green waste in Kenya. It involves the decomposition of organic waste through the action of microorganisms, resulting in nutrient-rich compost that can be used as a soil conditioner or fertilizer. Numerous composting initiatives have been established, ranging from small-scale household composting to large-scale commercial composting facilities. These initiatives not only divert green waste from landfills but also contribute to soil health and reduce the need for chemical fertilizers.
+   
+  2. Biogas Production:
+  Kenya has seen a rise in the implementation of biogas production systems, particularly in rural areas. Biogas is generated through the anaerobic digestion of organic waste, including green waste, animal manure, and kitchen waste. Biogas plants utilize this process to produce biogas, a renewable energy source that can be used for cooking, heating, and even electricity generation. By harnessing green waste for biogas production, these systems offer an eco-friendly solution while reducing reliance on traditional sources of energy.
+   
+  3. Green Waste Recycling:
+  Recycling initiatives are gaining traction in Kenya, with a focus on green waste recycling. Companies and organizations have introduced innovative techniques to process and recycle green waste into useful products. For example, some companies convert green waste into high-quality organic fertilizers, mulch, or biomass fuel pellets. These recycled products help improve soil fertility, conserve water, and reduce the use of chemical inputs in agriculture.
+   
+  4. Community-based Initiatives:
+  Kenya has witnessed the emergence of community-based initiatives that actively involve local residents in managing green waste. Community composting projects, where households collect and compost their organic waste collectively, have been successfully implemented in various neighborhoods. These initiatives not only foster a sense of community ownership but also educate individuals about waste management practices, promoting a culture of sustainability.
+   
+  5. Public-Private Partnerships:
+  Collaboration between the public and private sectors is crucial for effective green waste management in Kenya. Several partnerships have been formed between local authorities, waste management companies, and non-governmental organizations (NGOs) to develop comprehensive waste management systems. These partnerships facilitate the collection, segregation, and proper disposal or recycling of green waste. Furthermore, they often incorporate public awareness campaigns to educate citizens on the importance of responsible waste management.
+   
+  Conclusion:
+  Kenya is actively addressing the challenges associated with green waste management by implementing innovative and sustainable solutions. Through composting, biogas production, recycling, community-based initiatives, and public-private partnerships, the country is promoting environmentally friendly practices while reducing the strain on landfills and mitigating greenhouse gas emissions. These solutions not only offer practical waste management strategies but also contribute to the development of a greener and more sustainable future for Kenya. By encouraging further research, investment, and widespread adoption of these solutions, Kenya can continue to make significant progress in green waste management and environmental conservation.`
+
+},
+{
+  id: "gcks6543jg",
+  heading: "FOOD WASTE UNVEILED",
+  date: "January 19, 2023",
+  blogText: `Food Waste Unveiled: Insights from Influencers Making a Difference
+  Social media influencers hold immense sway in the digital realm, using their platforms to advocate for causes that matter. When it comes to food waste, these influential voices play a pivotal role in raising awareness, sharing tips, and inspiring their followers to make a positive impact. In this blog, we'll delve into the thoughts and advice of notable social media influencers who are passionate about tackling food waste. Their quotes shed light on the issue and provide practical solutions for individuals and communities to combat this global problem.
+ 
+  1. Jane Green: @SustainableJane
+  "I believe that every meal counts. By planning our grocery shopping, utilizing leftovers creatively, and composting food scraps, we can significantly reduce food waste. Let's embrace a more mindful approach to cooking and consumption, making a difference one plate at a time." - Jane Green
+   
+  2. Sam EcoWarrior: @SamEcoWarrior
+  "Food waste is a pressing issue that demands action. When we understand the impact of wasted food on hunger, the environment, and our wallets, we can't ignore it. I encourage my followers to start small, shop smart, and support local initiatives that rescue surplus food. Together, we can create a world with less waste and more compassion." - Sam EcoWarrior
+   
+  3. Lily Sustain: @LilySustainLife
+  "As an influencer, I feel a responsibility to shed light on the magnitude of food waste. It's not just about cleaning our plates; it's about changing our entire food system. Let's learn to value food, reduce our portion sizes, and educate others about the impact of waste. Together, we can create a more sustainable and equitable future." - Lily Sustain
+   
+  4. Mark WasteNot: @WasteNotMark
+  "Food waste is a global issue that requires individual action. By properly storing food, understanding expiration dates, and getting creative with leftovers, we can make a significant difference. I encourage my followers to join me in this journey towards zero waste, saving money while protecting the environment." - Mark WasteNot
+   
+  5. Mia FoodRescuer: @MiaFoodRescuer
+  "Food waste is not just about what we throw away; it's about the resources wasted throughout the entire food supply chain. I advocate for conscious shopping, supporting local farmers, and embracing imperfect produce. Let's rescue surplus food, donate to food banks, and raise awareness about this solvable issue." - Mia FoodRescuer
+   
+  6. Alex SustainableKitchen: @SustainableKitchen
+  "In my kitchen, sustainability is key. I strive to use every part of an ingredient, from root to stem, and incorporate food preservation techniques like freezing and pickling. Through my content, I hope to inspire others to embrace a zero-waste mindset, redefining how we view and interact with our food." - Alex SustainableKitchen
+   
+  Conclusion:
+  Social media influencers are at the forefront of driving change when it comes to food waste. Their insights and practical tips encourage individuals to rethink their consumption habits, embrace sustainable practices, and become part of the solution. By following their guidance—planning meals, reducing portion sizes, utilizing leftovers creatively, supporting local initiatives, and rescuing surplus food—we can collectively make a significant impact in reducing food waste. Let's leverage the power of social media and the wisdom of these influencers to create a more sustainable food system and a brighter future for all.
+`
+},
+]
--- src/Data/HomeSliderData.js
+++ src/Data/HomeSliderData.js
@@ -2,30 +2,30 @@ export const SlideData = [
   {
     imgUrl:
       "https://images.unsplash.com/photo-1584270060326-bac9f7520e98?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzI2fHxlbnZpcm9ubWVudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
-    heading: "Not a Lorem Ipsum whatsoever, but your Lorem Ipsum!",
+    heading: "Managing food waste",
     caption:
-      "Lorem ipsum dolor sit amet. Aut modi alias ea asperiores inventore non libero veritatis qui quidem exercitationem qui voluptas facere et odit voluptatum.culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga culpa qui official",
+      "Managing food waste isn't just about reducing our environmental footprint; its an opportunity to unleash our creativity. W ith innovative strategies and a dash of imagination, we can transform what might have been wasted to a feast worth savoring. Let's make every meal count and turn waste into a culinary adventure",
   },
   {
     imgUrl:
       "https://plus.unsplash.com/premium_photo-1661326204481-9a97c1b1627e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mzc5fHxlbnZpcm9ubWVudHxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=400&q=60",
-    heading: '"Finibus Bonorum et Malorum", written by Cicero in 45 BC',
+    heading: "impact of green waste on environment",
     caption:
-      "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga et dolorum fuga culpa qui official",
-  },
+      "Green waste, seemingly innocuos, holds the power to shape the health of our environment. From the decomposition process that emitspotent greenhouse gases to the loss of valuable resources, the consequences are far reaching. Join us as we explore sustainable solutions to preserve our planet's beauty. Together. let's pave the way towards a greener, more vibrant future"
+    },
   {
     imgUrl:
       "https://images.unsplash.com/photo-1501696057772-02661ce93702?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
-    heading: "Non assumenda velit eos facere officiis ut commodi omnis qui",
+    heading: "put waste to good use",
     caption:
-      "Et ipsum explicabo qui necessitatibus tenetur et veniam soluta est culpa adipisci. Sed eveniet similique ut nemo iste vel modi neque id possimus maiores in earum sunt est accusantium dolores id consequuntur facilis.culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga culpa qui official",
-  },
+      "In a world overflowing with waste, it's time to tap into the potential hiding within our discarded treasures, rather than letting waste suffocate our environment.Let's embark ona journey of resourcefullnessand dicover the hidden value within. FRom repursing to recycling, we have the power to transform waste into something extraordinary."
+    },
   {
     imgUrl:
       "https://images.unsplash.com/photo-1536423871287-6bb919568b58?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NTB8fGVudmlyb25tZW50fGVufDB8fDB8fHww&auto=format&fit=crop&w=400&q=60",
     heading:
-      "Aut culpa velit est excepturi optio aut consequatur minus est esse maxime",
+      "global fight against food waste",
     caption:
-      "Ut explicabo molestiae et dolore nulla 33 totam voluptatem eos voluptas excepturi id doloribus soluta est suscipit suscipit est galisum voluptate.culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga et dolorum fuga culpa qui official",
-  },
+      "In a world where millions go hungry, a silent battle is being fought to eradicate food waste and nourish those in need. From bustling cities to remote villages, the global fight against food waste unites us in a common mission: to ensure no plate is left empty and no morsel goes to waste. join us as we empower communities, and rewrite the narrative of hunger. It's time to foster resilience, and create a world where every person can savor the taste of a brighter tomorrow."
+     },
 ];
