07fbad837ba8facb1b16546eb54ac294fed78ffa
devopati <davidopati70@gmail.com>
2023-07-01 13:27:11 +0300

    n

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import React from "react";
 import { WebRoutes as Routes } from "./Routes/Routes";
 import { Store } from "./Redux/Store";
 import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 const App = () => {
   return (
     <>
       <Provider store={Store}>
+        <ToastContainer />
         <Routes />
       </Provider>
     </>
